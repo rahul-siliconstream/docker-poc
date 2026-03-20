3e57c34430498c2b18b1f121ba84bc0f94da049f
Aman Madhukar <amanmadhukar@MacBook-Air.local>
2026-03-20 17:38:13 +0530

    string literal issue

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ else{
   });
 
   app.get('/pid', (req, res) => {
-    res.send('Req handled by worker: ',process.pid,' CPU Core Count: ',CPU_CORE_COUNT);
+    res.send(`Req handled by worker: ${process.pid}, CPU Core Count: ${CPU_CORE_COUNT}`);
   });
   
   app.listen(3000);
